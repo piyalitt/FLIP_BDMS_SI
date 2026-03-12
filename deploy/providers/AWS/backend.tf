7439ae4795a8e3138a8b1b0612e5c68f1facd6eb
at24_bioeng625-pc <alexandre.triay_bagur@kcl.ac.uk>
2026-03-12 13:13:40 +0000

    revert

--- deploy/providers/AWS/backend.tf
+++ deploy/providers/AWS/backend.tf
@@ -14,9 +14,9 @@
 
 terraform {
   backend "s3" {
-    key          = "flip/terraform.tfstate"
-    region       = "eu-west-2"
-    encrypt      = true
+    key     = "flip/terraform.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
     use_lockfile = true
   }
 }
